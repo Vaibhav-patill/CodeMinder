eda0d4cad7c55da769701fcf0059937b9ae26828
Vaibhav-patill <vaibhavpatil00913@gmail.com>
2025-04-07 21:45:15 +0530

    changes made

--- Backend/Model/GeneralNotes.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const GeneralNoteSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  noteName: { type: String, required: true },
-  detail: { type: String, required: true },
-});
-
-export default mongoose.model("GeneralNote", GeneralNoteSchema);
--- Backend/Model/Notes.js
+++ Backend/Model/Notes.js
@@ -2,8 +2,14 @@ import mongoose from "mongoose";
 
 const NoteSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  question_id: { type: mongoose.Schema.Types.ObjectId, ref: "Question", required: true },
+  type: {
+    type: String,
+    enum: ["question", "general"],
+    required: true,
+  },
+  question_id: { type: mongoose.Schema.Types.ObjectId, ref: "Question" },
+  noteName: { type: String },
   content: { type: String, required: true },
-});
+}, { timestamps: true });
 
-export default mongoose.model("Notes", NoteSchema);
+export default mongoose.model("Note", NoteSchema);
