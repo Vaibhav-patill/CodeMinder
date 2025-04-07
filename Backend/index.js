f7eb263bb29851bd280d07b06c3b1ec876803b0e
Vaibhav-patill <vaibhavpatil00913@gmail.com>
2025-04-07 21:43:57 +0530

    changes made

--- Backend/index.js
+++ Backend/index.js
@@ -1,45 +1,50 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
 import connectDB from "./Config/Connection.js";
-import UserRouter from "./Routes/User.js"
-import SheetRouter from "./Routes/Sheet.js"
-import NoteRouter from "./Routes/Note.js"
-import GithubRouter from "./Routes/Github.js"
-import LeetcodeRouter from "./Routes/Leetcode.js"
-import CodeforcesRouter from "./Routes/Codeforces.js"
-import GFGRouter from "./Routes/GFG.js"
-import AIInterview from "./Routes/interviewRoutes.js"
-import { checkAndSendEmails } from "./Controller/Mail.js";
+
+import UserRouter from "./Routes/User.js";
+import SheetRouter from "./Routes/Sheet.js";
+import NoteRouter from "./Routes/Note.js";
+import ProfileRouter from "./Routes/Profile.js";
+
+import AIInterview from "./Routes/interviewRoutes.js";
+import AnalyzeResume from "./Routes/ResumeAnalyze.js";
+// import { checkAndSendEmails } from "./Controller/Mail.js";
 import { authenticateToken } from "./Middlewares/Auth.js";
-import { githubData } from "./Controller/Aiagent.js";
+import { generateAIResponse } from "./Controller/Aiagent.js";
 import { FetchInternships } from "./Controller/InternshipController.js";
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 const URI = process.env.MONGODB_URI;
 const app = express();
-connectDB(URI)
+connectDB(URI);
 
 //Middlerwares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Check Route
 app.get("/health", (req, res) => {
-    res.send("OK");
+  res.send("OK");
 });
-app.use("/user", UserRouter);
+
+// Routes
+app.use("/api/user", UserRouter);
 app.use("/api/sheets", SheetRouter);
 app.use("/api/notes", NoteRouter);
-app.use("/api/github", GithubRouter);
-app.use("/api/leetcode", LeetcodeRouter);
-app.use("/api/codeforces", CodeforcesRouter);
-app.use("/api/gfg", GFGRouter);
-app.use("/api/v1/AiInterview",AIInterview);
-app.post("/api/gemini",authenticateToken, githubData);
-app.get("/api/intership", FetchInternships);
+
+app.use("/api/profile", ProfileRouter);
+app.post("/api/aiagent", authenticateToken, generateAIResponse);
+
+app.use("/api/aiinterview", AIInterview);
+app.use("/api/resume", AnalyzeResume);
+
+app.get("/api/jobs", FetchInternships);
+
 // checkAndSendEmails()
 app.listen(PORT, () => {
-    console.log("Server is running on " + PORT);
+  console.log("Server is running on " + PORT);
 });
